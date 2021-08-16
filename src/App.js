916a0e932d73690aac8ebf526a4a912201271310
d4nielj <d4niel.djm@gmail.com>
2021-08-16 12:09:38 -0500

    Add TodoContainer file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import TodoContainer from './Components/TodoList/todoContainer';
+
 function App() {
   return (
     <div className="App">
       <header>
         <h1>Todo List</h1>
       </header>
+      <TodoContainer />
     </div>
   );
 }
---
+++ src/Components/TodoList/todoContainer.js
@@ -0,0 +1,14 @@
+const TodoContainer = () => {
+  return (
+    <div className="todo-container">
+      <h2>Your list</h2>
+      <ul>
+        <li>item</li>
+        <li>item</li>
+        <li>item</li>
+      </ul>
+    </div>
+  );
+};
+
+export default TodoContainer;
